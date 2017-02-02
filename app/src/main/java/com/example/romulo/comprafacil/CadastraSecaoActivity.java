2b3a3ffdd4f0e74f0fb537c0341362fe7ecb3b7b
Administrador <pedrocesar1010@gmail.com>
2017-02-02 19:34:47 -0300

    Adicionado telas de cadastro.

---
+++ app/src/main/java/com/example/romulo/comprafacil/CadastraSecaoActivity.java
@@ -0,0 +1,13 @@
+package com.example.romulo.comprafacil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastraSecaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastra_secao);
+    }
+}
