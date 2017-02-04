8401ea9dff0c55cf5790a72a7e037dd3d25b8a8e
Administrador <pedrocesar1010@gmail.com>
2017-02-03 23:45:51 -0300

    Adicionado função de inserir e listar.

--- app/src/main/java/com/example/romulo/comprafacil/CadastraProdutoActivity.java
+++ app/src/main/java/com/example/romulo/comprafacil/CadastraProdutoActivity.java
@@ -1,7 +1,13 @@
 package com.example.romulo.comprafacil;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.romulo.comprafacil.DAO.ProdutoDAO;
 
 public class CadastraProdutoActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class CadastraProdutoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastra_produto);
+
+        final ProdutoDAO PD = new ProdutoDAO(this);
+
+    //   Botao para setar dados do banco da tela produto
+        Button botao = (Button) findViewById(R.id.cadrasProduto);
+
+        final EditText codigo = (EditText) findViewById(R.id.editTextCodProd1);
+        final EditText nome = (EditText) findViewById(R.id.editTextNomProd1);
+        final EditText localizacao = (EditText) findViewById(R.id.editTextSecao);
+
+       // final String oi = String.valueOf(codigo.getText());
+
+        final String nomeString = nome.getText().toString();
+     //   final String localizacaoString = localizacao.getSelectedItem().toString();
+
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  PD.inserirProduto(codigoString,nomeString,"localizacaoString");
+                String codigoString = codigo.getText().toString();
+                String nomeString = nome.getText().toString();
+                String localizacaoString = localizacao.getText().toString();
+                //String oi = String.valueOf(codigo.getText());
+                //AlertDialog.Builder al = new AlertDialog.Builder(CadastraProdutoActivity.this);
+               // al.setMessage(">"+codigoString+"r"+nomeString+oi);
+                //al.show();
+                PD.inserirProduto(codigoString,nomeString,localizacaoString);
+                finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/romulo/comprafacil/MainActivity.java
+++ app/src/main/java/com/example/romulo/comprafacil/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.romulo.comprafacil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
 //            public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
+//            }
+//        });
+
+//        final SearchView pesquisa = (SearchView) findViewById(R.id.pesquisa);
+//        pesquisa.setOnSearchClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String result = pesquisa.getQuery().toString();
+//                AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+//                alerta.setMessage(result);
+//                alerta.show();
+//
 //            }
 //        });
     }
