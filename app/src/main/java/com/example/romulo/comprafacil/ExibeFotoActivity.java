5f9e0a8b13cbc1d9ed6af8798481e326265edb63
Administrador <pedrocesar1010@gmail.com>
2017-02-13 21:30:18 -0300

    Finais.

---
+++ app/src/main/java/com/example/romulo/comprafacil/ExibeFotoActivity.java
@@ -0,0 +1,13 @@
+package com.example.romulo.comprafacil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExibeFotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exibe_foto);
+    }
+}
